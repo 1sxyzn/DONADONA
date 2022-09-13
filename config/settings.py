09110d921429bc6cfa2e96d40addcc36f07eb0e0
Soyun Won <soyun9902@ewhain.net>
2022-09-13 22:34:17 +0900

    DB: #18 Connect RDS (MySQL)

--- config/settings.py
+++ config/settings.py
@@ -73,8 +73,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config("DATABASE_NAME"),
+        'USER': config("DATABASE_USERNAME"),
+        'PASSWORD': config("DATABASE_PASSWORD"),
+        'HOST': config("DATABASE_HOST"),
+        'PORT': config("DATABASE_PORT", cast=int),
     }
 }
 
